e17eb22531a67ab67286acb94fb302b576314623
ehach404 <pawan073bct@ioepc.edu.np>
2024-06-23 23:54:56 +0545

    varkar suru

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application,Request,Response } from 'express'
+const app:Application = express()
+const PORT:number= 3001;
+
+require("./model/index")
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send("Hello world");
+});
+app.get("/about",(req:Request,res:Response)=>{
+      res.send("About page");
+});
+app.listen(PORT,()=>{ 
+    console.log("Server has started at port",PORT);
+});
