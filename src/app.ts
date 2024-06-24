ce79118fca117f93fd0f3ab936b644efcf157454
ehach404 <pawan073bct@ioepc.edu.np>
2024-06-25 00:31:57 +0545

    add gareko

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import express, { Application,Request,Response } from 'express'
 const app:Application = express()
 const PORT:number= 3001;
 
-require("./model/index")
 
-app.get("/",(req:Request,res:Response)=>{
-    res.send("Hello w orld");
-});
-app.get("/about",(req:Request,res:Response)=>{
-      res.send("About page");
-});
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
+import './database/connection'
+
+import userRoute from './Routes/userRoute'
+app.use(express.json())
+app.use ("", userRoute)
 app.listen(PORT,()=>{ 
     console.log("Server has started at port",PORT);
 });
---
+++ src/database/models/userModel.ts
@@ -0,0 +1,39 @@
+import { table } from 'console';
+import {
+Table,
+Column,
+Model,
+DataType,
+CreatedAt,
+PrimaryKey
+} from 'sequelize-typescript'
+
+
+@Table({
+    tableName: 'users',
+    modelName : 'User',
+    timestamps : true
+})
+
+class User extends Model{
+    @Column({
+        primaryKey : true,
+        type : DataType.UUID,
+        defaultValue: DataType.UUIDV4
+    })
+    declare id:string;
+
+    @Column({
+        type: DataType.STRING
+    })
+    declare username : string
+    @Column({
+        type: DataType.STRING
+
+    })declare email:string
+
+    @Column({
+        type: DataType.STRING
+    }) declare password : string
+}
+export default User
